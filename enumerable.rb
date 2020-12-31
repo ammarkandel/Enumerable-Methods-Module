7df0766943541b1c2346638f262898e7b48200e3
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2020-12-31 19:02:51 -0400

    modify my_inject, my_map and my_none

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,6 @@
+# rubocop:disable Metrics/ModuleLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
@@ -69,28 +72,18 @@ module Enumerable
     true
   end
 
-  def my_none?(param = nil)
-    if !block_given? && param.nil?
-      my_each { |num| return true if num }
-      return false
+  def my_none?(*param)
+    return "`my_none': wrong arguments(given #{arguments.length}, expected 0..1)" if arguments.length > 1
+
+    if block_given?
+      my_each { |num| return false if yield(num) }
+    elsif param.empty?
+      my_each { |num| return false unless num.nil? || num == false}
+    elsif param[0].is_a? Regexp
+      my_each { |num| return false if param[0].match(num) }
+    else
+      my_each { |num| return false if num == param[0] }
     end
-
-    if !block_given? && !param.nil
-
-      if param.is_a?(Class)
-        my_each { |num| return false if num.instance_of?(param) }
-        return true
-      end
-
-      if param.instance_of?(Regexp)
-        my_each { |num| return false if arg.match(num) }
-        return true
-      end
-
-      my_each { |num| return false if num == param }
-      true
-    end
-    my_any? { |it| return false if yield(it) }
     true
   end
 
@@ -105,75 +98,43 @@ module Enumerable
 
   def my_map(par_ = nil)
     new_arr = []
+    return to_enum unless block_given?
+
     if par_
-      each { |i| new_arr << par_.call(i) }
+      my_each { |i| new_arr << par_.call(i) }
     else
-      each { |i| new_arr << yield(i) }
-      new_arr
+      my_each { |i| new_arr << yield(i) }
     end
+    new_arr
   end
 
-  def my_inject(number = nil, _syn = nil)
+  def my_inject(number = nil, syn = nil)
     if block_given?
       acc = number
       my_each { |i| acc = acc.nil? ? i : yield(acc, i) }
+      raise LocalJumperError unless block_given? || !syn.empty? || !number.empty?
+
       acc
     elsif !number.nil? && (number.is_a?(Symbol) || number.is_a?(String))
+      raise LocalJumperError unless block_given? || !number.empty?
+
       acc = nil
       my_each { |i| acc = acc.nil? ? i : acc.send(number, i) }
       acc
-    elsif !sym.nil? && (sym.is_a?(Symbol) || sym.is_a?(String))
+    elsif !syn.nil? && (syn.is_a?(Symbol) || syn.is_a?(String))
+      raise LocalJumperError unless block_given? || !syn.empty?
+
       acc = number
-      my_each { |i| acc = acc.nil? ? i : acc.send(sym, i) }
-    else
+      my_each { |i| acc = acc.nil? ? i : acc.send(syn, i) }
       acc
+    else
+      raise LocalJumperError
     end
   end
-
-  puts 'my_each if type Array'
-  puts [1, 2, 3, 4].my_each(&:even?)
-
-  puts 'my_each if type Range'
-  puts (0..5).my_each(&:even?)
-
-  puts 'my_each_with_index if type Array'
-  [5, 6, 7, 8].my_each_with_index do |i, index|
-    puts "#{i} have index #{index}"
-  end
-
-  puts 'my_each_with_index if type Range'
-  (8..15).my_each_with_index do |i, index|
-    puts "#{i} have index #{index}"
-  end
-
-  puts 'my select for any type'
-  puts [8, 90, 6, 3, 4, 1].my_select(&:even?)
-
-  puts 'check my all'
-  puts [2, 4, 6, 8].my_all?(&:even?)
-
-  puts 'check if any of my'
-  puts [1, 3, 5, 7].my_all?(&:even?)
-
-  puts 'check if none of my'
-  puts [1, 3, 5, 8].my_none?(&:even?)
-
-  num = %w[a s f r].count('n')
-  puts num
-
-  num = %w[a s f r].my_count('n')
-  puts num
-
-  p([1, 2, 3, 4].map { |i| i * i })
-  p([1, 2, 3, 4].my_map { |i| i * i })
-
-  p((0..5).map { |i| i * i })
-  p((0..5).my_map { |i| i * i })
-
-  my_proc = proc { |i| i * i }
-  p [0, 1, 2, 3, 4].map(&my_proc)
-  p [0, 1, 2, 3, 4].my_map(&my_proc)
-
-  p((5..10).my_inject { |x, y| x + y })
-  p([5, 5, 7, 8].my_inject(1) { |x, y| x * y })
+end
+# rubocop:disable Metrics/ModuleLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+def multiply_els(array)
+  array.my_inject(:*)
 end
