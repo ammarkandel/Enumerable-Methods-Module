9743943b8f4fbcb3fbbc1657901768457c1bc12b
ammarkandel <merom8703@gmail.com>
2021-01-21 17:40:44 +0200

    add negative tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,4 +1,4 @@
-require './lib/enumerable'
+require '../lib/enumerable'
 
 describe Enumerable do
   let(:range) { (1..9) }
@@ -16,6 +16,10 @@ describe Enumerable do
     it 'expect my each to be enumarator' do
       expect(test_arr_two.my_each).to be_an(Enumerator)
     end
+
+    it 'expect my each to not eql range from 1 to 9' do
+      expect(range.my_each { |i| i }).not_to eql((15..20))
+    end
   end
 
   describe '#my_select' do
@@ -26,40 +30,52 @@ describe Enumerable do
     it 'expect my each to eql selected items' do
       expect(test_arr_two.my_select(&:even?)).to eql(test_arr_two.my_select(&:even?))
     end
+
+    it 'expect my each to eql selected items' do
+      expect(test_arr_two.my_select(&:even?)).not_to eql(test_arr_two.my_select(&:odd?))
+    end
   end
 
   describe '#my_any?' do
-    it 'expect my_any to return true' do
+    it 'expect my_any to return true if the argument in the array' do
       expect([4, 5, true, 6].my_any?(true)).to eql(true)
     end
 
-    it 'expect my_any to return false' do
+    it 'expect my_any to return true if false in the array' do
       expect([4, 5, false, 6].my_any?(false)).to eql(true)
     end
 
-    it 'expect my_any to return false' do
+    it 'expect my_any to return false if argument not in the array' do
       expect([4, 5, false, 6].my_any?(10)).to eql(false)
     end
 
-    it 'expect my_any to return true' do
+    it 'expect my_any to return true if the argument in the array' do
       expect([4, 5, 20, 6].my_any?(20)).to eql(true)
+    end
+
+    it 'expect my_any to return true if the argument in the range' do
+      expect((1..20).my_any?(20)).to eql(true)
+    end
+
+    it 'expect my_any to return false if the argument not in the range' do
+      expect((1..20).my_any?(30)).to eql(false)
     end
   end
 
   describe '#my_all' do
-    it 'expect my_all to return true' do
+    it 'expect my_all to return true if all items equal the argument' do
       expect(test_arr_one.my_all?(1)).to eql(true)
     end
 
-    it 'expect my_all to return false' do
+    it 'expect my_all to return false if not all items equal the argument' do
       expect([1, 1, 1, 2].my_all?(1)).to eql(false)
     end
 
-    it 'expect my_all to return false' do
+    it 'expect my_all to return false if not all items greater than 10' do
       expect(test_arr_one.my_all? { |i| i > 10 }).to eql(false)
     end
 
-    it 'expect my_all to return true' do
+    it 'expect my_all to return true if all item less than 10' do
       expect(test_arr_one.my_all? { |i| i < 10 }).to eql(true)
     end
   end
@@ -72,6 +88,10 @@ describe Enumerable do
     it 'expect my_map to return true' do
       expect((1..4).my_map { |i| i * 1 }).to eql([1, 2, 3, 4])
     end
+
+    it 'expect my_map to not be this array' do
+      expect((1..4).my_map { |i| i * 1 }).not_to eql([1, 2, 3, 5])
+    end
   end
 
   describe '#my_each_with_index' do
@@ -81,6 +101,10 @@ describe Enumerable do
 
     it 'expect my each to eql range from 1 to 9' do
       expect(range.my_each_with_index { |i| i }).to eql(range)
+    end
+
+    it 'expect my each to not eql range from 1 to 9' do
+      expect(range.my_each_with_index { |i| i }).not_to eql((15..20))
     end
 
     it 'expect my each to be enumarator' do
@@ -95,6 +119,10 @@ describe Enumerable do
 
     it 'expect my count to return 2' do
       expect([1, 2, 2, 6, 8, 9].my_count { |item| item == 2 }).to eql(2)
+    end
+
+    it 'expect my count to return 2' do
+      expect([1, 2, 2, 6, 8, 9].my_count { |item| item == 2 }).not_to eql(4)
     end
   end
 
@@ -128,6 +156,10 @@ describe Enumerable do
 
     it 'test if range given' do
       expect(range.my_inject { |i| i == false }).to eql(true)
+    end
+
+    it 'expect my_inject to accept sympol and number' do
+      expect(arr.my_inject(0, :+)).not_to eql(12)
     end
   end
 end
